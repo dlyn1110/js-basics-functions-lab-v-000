9937ba83ac833ce70cf7ac68bb1d62535759af7e
Dani Lyn <dlyn1110@gmail.com>
2019-01-24 05:00:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ function distanceFromHqInBlocks(block){
 function distanceTravelledInFeet(start, destination) {
   return distanceTravelledInFeet(destination) - distanceTravelledInFeet(start);
 }
+
+ function calculatesFarePrice() {
+   feet = distanceTravelledInFeet(start, destination);
+   if (feet <= 400) {
+     return 0;
+   } else if (feet > 400 && feet < 2000) {
+      const charged = feet - 400;
+      const price = charged * 0.02;
+      return price;
+   } else if (feet > 2000 && feet < 2500) {
+     return 25;
+   } else {
+     return 'cannot travel that far';
+   }
+ }
